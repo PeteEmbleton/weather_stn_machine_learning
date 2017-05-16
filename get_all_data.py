40609b941735512caa1e8c30436b2f4758cdeeaf
Peter Embleton <pembleto@usc.edu.au>
2017-05-17 06:53:02 +1000

    no message

--- get_all_data.py
+++ get_all_data.py
@@ -38,7 +38,7 @@ with urllib.request.urlopen("https://apex.oracle.com/pls/apex/raspberrypi/weathe
             next_url = station_data['next']['$ref'] #get the next url
 
             for j in range(len(station_data['items'])):
-                #need to try all th fields as some may be missing
+                #need to try all the fields as some may be missing
 
                 try:
                     stn_id = (station_data['items'][j]['weather_stn_id'])
@@ -111,26 +111,26 @@ with urllib.request.urlopen("https://apex.oracle.com/pls/apex/raspberrypi/weathe
                     rainfall = (station_data['items'][j]['rainfall'])
 
                 try:
-                    time_to_convert = str(all_stations['items'][i]['reading_timestamp'])
-                    reading_timestamp = str((parse(time_to_convert))).split('+', 1)[0]
+                    time_to_convert = str(all_stations['items'][j]['reading_timestamp'])
+                    reading_timestamp_out = str((parse(time_to_convert))).split('+', 1)[0]
 
                 except:
                     pass
                 else:
-                   time_to_convert = str(all_stations['items'][i]['reading_timestamp'])
-                   reading_timestamp = str((parse(time_to_convert))).split('+', 1)[0]
+                   time_to_convert = str(all_stations['items'][j]['reading_timestamp'])
+                   reading_timestamp_out = str((parse(time_to_convert))).split('+', 1)[0]
                 time.sleep(0.5)
 
-                #print(stn_id, reading_timestamp)
-                data_exists = (cursor.execute("""SELECT reading_timestamp FROM weather_stn WHERE weather_stn_id = '%s' and reading_timestamp = '%s'""" %(stn_id,reading_timestamp)))
+                #print(stn_id, time_to_convert, reading_timestamp_out, "j = ",j)
+                data_exists = (cursor.execute("""SELECT reading_timestamp FROM weather_stn WHERE weather_stn_id = '%s' and reading_timestamp = '%s'""" %(stn_id,reading_timestamp_out)))
                 #print(data_exists)
 
                 if data_exists == 0:
-                    stn_data = (stn_id, ambient_temp, ground_temp, air_quality, air_pressure, humidity, wind_direction, wind_speed, wind_gust_speed, rainfall, reading_timestamp)
+                    stn_data = (stn_id, ambient_temp, ground_temp, air_quality, air_pressure, humidity, wind_direction, wind_speed, wind_gust_speed, rainfall, reading_timestamp_out)
                     cursor.execute(update_record, stn_data)
                     db.commit()
-                   # print("create record")
-                    count += 1
+                    #print("create record")
+                    counter += 1
 
                 while continue_loop == 1:
                     try:
@@ -220,27 +220,28 @@ with urllib.request.urlopen("https://apex.oracle.com/pls/apex/raspberrypi/weathe
 
                                 try:
                                     time_to_convert = str(all_stations['items'][k]['reading_timestamp'])
-                                    reading_timestamp = str((parse(time_to_convert))).split('+', 1)[0]
+                                    reading_timestamp_out = str((parse(time_to_convert))).split('+', 1)[0]
                                 except:
                                     pass
                                 else:
-                                    time_to_convert = str(all_stations['items'][i]['reading_timestamp'])
-                                    reading_timestamp = str((parse(time_to_convert))).split('+', 1)[0]
+                                    time_to_convert = str(all_stations['items'][k]['reading_timestamp'])
+                                    reading_timestamp_out = str((parse(time_to_convert))).split('+', 1)[0]
 
                                 stn_id = (station_data['items'][k]['weather_stn_id'])
 
 
-                               # print(stn_id, reading_timestamp)
-                                data_exists = (cursor.execute("""SELECT reading_timestamp FROM weather_stn WHERE weather_stn_id = '%s' and reading_timestamp = '%s'""" %(stn_id,reading_timestamp)))
-                               # print(data_exists)
+                                #print(stn_id, time_to_convert, reading_timestamp_out, "k = ",k)
+                                data_exists = (cursor.execute("""SELECT reading_timestamp FROM weather_stn WHERE weather_stn_id = '%s' and reading_timestamp = '%s'""" %(stn_id,reading_timestamp_out)))
+                                #print(data_exists)
 
                                 if data_exists == 0:
-                                    stn_data = (stn_id, ambient_temp, ground_temp, air_quality, air_pressure, humidity, wind_direction, wind_speed, wind_gust_speed, rainfall, reading_timestamp)
+                                    stn_data = (stn_id, ambient_temp, ground_temp, air_quality, air_pressure, humidity, wind_direction, wind_speed, wind_gust_speed, rainfall, reading_timestamp_out)
                                     cursor.execute(update_record, stn_data)
                                     db.commit()
                                     counter += 1
-                                   # print("create record")
+                                    #print("create record")
 
     cursor.close()
     db.close()
 print("weekly updated records ",counter)
+
